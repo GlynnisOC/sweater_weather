d2ab5f2f915c9e5ca2085a5c711cea664b9d2d3b
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-15 14:56:54 -0600

    Add route for forecasts controller

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe '/api/v1/forecast' do
   it 'returns the forecast for a specific city' do
-    
+    headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+
+    get '/api/v1/forecast', params: {location: 'denver,co'}, headers: headers
+    expect(status_code).to eq(200)
   end
 end
 
