b1af1856547ad73cbc957fbdd853fed6f6946eb1
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-15 14:20:32 -0600

    Add darksky api key and figaro to gemfile

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -1,3 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 
+RSpec.describe '/api/v1/forecast' do
+  it 'returns the forecast for a specific city' do
+    
+  end
+end
+
+
+# GET /api/v1/forecast?location=denver,co
+# Content-Type: application/json
+# Accept: application/json
