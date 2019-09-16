aee21fce3763b450c1da86e4f3104938943e1897
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-16 09:28:50 -0600

    Add location into params to return daily summary for day in specific location

---
+++ spec/requests/api/v1/gifs_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe '/api/v1/gifs' do
+  it "returns a random gif based on day's forecast conditions" do
+    headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+
+    get '/api/v1/gifs', params: {location: 'denver, co'}, headers: headers
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq('application/json')
+  end
+end
+
+# /api/v1/gifs?location=denver,co
