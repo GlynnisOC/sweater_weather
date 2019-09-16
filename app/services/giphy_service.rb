858901842a743f4c3d1b7d2c1e461d0d13feb3ab
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-16 10:53:34 -0600

    Add q as part of service that pulls out the day's summary

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,16 +1,22 @@
 class GiphyService
 
   def gifs(forecast)
-    binding.pry
-    eight_conditions = forecast[:daily][:data]
-    eight_conditions.map do |daily|
+    conditions = forecast[:daily][:data]
+    day_objects = conditions.map do |daily|
       Daily.new(daily)
     end
+    q = day_objects.map do |day|
+      day.summary
+    end
+    binding.pry
+    url = "/v1/gifs/search"
+    params = { api_key: ENV['GIPHY-API-KEY'], q: q } 
+    get_json(url, params)
   end
 
   private
   def connection
-    @_connection ||= Faraday.new("http://api.giphy.com/v1/gifs/search")
+    @_connection ||= Faraday.new("http://api.giphy.com")
   end
 
   def get_json(url, params)
