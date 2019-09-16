6eb9cc3074c13ede79aa4da4f913bb9213d9efb7
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-16 09:28:03 -0600

    Add route for gifs controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'api/v1/forecast', to: 'api/v1/forecasts#show'
+  get 'api/v1/gifs', to: 'api/v1/gifs#random'
 end
