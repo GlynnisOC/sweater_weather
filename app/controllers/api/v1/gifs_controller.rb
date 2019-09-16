c2c6a33472c9f519f956b3e3f1a357f0cb40df73
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-16 09:49:42 -0600

    Add daily gifs method to gifs serializer

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::GifsController < ApplicationController
 
   def random
     location = Location.geocoded(params['location'])
+    render json: GifsSerializer.new(location).daily_gifs
   end
 end
