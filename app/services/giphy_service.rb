7c8a494798ea5f6fe3b39c2fa2c21388ca96e40b
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-16 09:50:14 -0600

    Add beginnings of connection and json being returned from connection

---
+++ app/services/giphy_service.rb
@@ -0,0 +1,12 @@
+class GiphyService
+
+  private
+  def connection
+    @_connection ||= Faraday.new("http://api.giphy.com/v1/gifs/search")
+  end
+
+  def get_json(url, params)
+    response = connection.get(url, params)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
