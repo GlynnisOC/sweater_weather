01fabdb4f14697939faedbde5667107039273c50
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-16 19:52:45 -0600

    Refactored so location is not being given on lat and long but location itself

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::BackgroundsController < ApplicationController
+
+  def show
+    background = BackgroundFacade.new(params['location']).background
+    render json: BackgroundSerializer.new(background)
+  end
+end
