68289f37eb8bc5380125c690e902e11488146399
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 11:20:41 -0600

    Previously monkeypatched solution without realizing api_key is already with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   validates :password, confirmation: true
   has_secure_password
 
-  after_initialize :api_key
+  after_initialize :create_api_key
 
-  def api_key
+  def create_api_key
     self.update(api_key: SecureRandom.hex)
   end
 end
