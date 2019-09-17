18c667136818c2240fb29d6f63f989aa64495ff0
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 17:49:13 -0600

    Add edge case testing to ensure status code of 400 is returned if any issues

--- spec/requests/api/v1/user_can_login_spec.rb
+++ spec/requests/api/v1/user_can_login_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe '/api/v1/sessions' do
     expect(user_data[:status]).to eq(200)
     expect(user_data).to have_key(:api_key)
   end
+
+  describe 'edge case testing' do
+    it 'returns an error message and a 400 status if incorrect credentials' do
+      user = User.create!('email': 'mousse@ballsrgreat.com', 'password': 'password', 'password_confirmation': 'password')
+
+      headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+      params = { "email": "mouse@ballsrgreat.com", "password": "#{user.password}" }
+
+      post '/api/v1/sessions', params: params.to_json, headers: headers
+
+      bad_request = JSON.parse(response.body, symbolize_names: true)
+      expect(bad_request[:status]).to eq(400)
+    end
+  end
 end
