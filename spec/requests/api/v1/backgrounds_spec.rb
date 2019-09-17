ed70e85384cfe81f60a58ebfab8b9b6a19c54d3d
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-16 18:55:15 -0600

    Add beginning of backgrounds spec

---
+++ spec/requests/api/v1/backgrounds_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe '/api/v1/backgrounds' do
+  it 'returns an image of the city whose forecast I want' do
+    headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+
+    get '/api/v1/backgrounds', params: {location: 'denver,co'}, headers: headers
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq('application/json')
+
+    background_data = JSON.parse(response.body, symbolize_names: true)
+
+    # expect(forecast_data).to be_a Hash
+    # expect(forecast_data[:data][:attributes][:time]).to eq('America/Denver')
+  end
+end
