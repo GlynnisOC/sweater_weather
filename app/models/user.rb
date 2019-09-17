4c4eba95a646860f6256eeccfaa6d8d330ca9ba2
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 10:37:57 -0600

    Add api key as a method for after initializing the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  validates_presence_of :email, :api_key
+  validates_presence_of :email
   validates :password, confirmation: true
   has_secure_password
 
+  after_initialize :api_key
+
+  def api_key
+    self.update(api_key: SecureRandom.hex)
+  end
 end
