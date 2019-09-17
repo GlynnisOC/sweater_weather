3c1ec8c7933024b52d73e5d3523a16e9d073e8e8
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 08:29:22 -0600

    Add spec for account creation

---
+++ spec/requests/api/v1/new_user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe '/api/v1/users' do
+  it 'returns an image of the city whose forecast I want' do
+    headers = {'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+    params = {"email": "mousse@ballsrgreat.com", "password": 'password', "password_confirmation": 'password' }
+
+    post '/api/v1/users', params: params, headers: headers
+    expect(response.status).to eq(201)
+    expect(response.content_type).to eq('application/json')
+
+    user_data = JSON.parse(response.body, symbolize_names: true)
+    expect(user_data).to be_a Hash
+
+  end
+end
+
+# Response:
+#
+# status: 201
+# body:
+#
+# {
+#   "api_key": "jgn983hy48thw9begh98h4539h4",
+# }
