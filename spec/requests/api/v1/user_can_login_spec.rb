2d8bf9015e95fcac020a1ba29f05d7bec92325c8
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 14:55:17 -0600

    Added expectation of key for response to return

--- spec/requests/api/v1/user_can_login_spec.rb
+++ spec/requests/api/v1/user_can_login_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe '/api/v1/sessions' do
   it 'returns a 200 status and users api key' do
+    user = User.create!('email': 'mousse@ballsrgreat.com', 'password': 'password', 'password_confirmation': 'password')
+
     headers = { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
-    params = { "email": "mousse@ballsrgreat.com", "password": 'password' }
+    params = { "email": "#{user.email}", "password": "#{user.password}" }
 
     post '/api/v1/sessions', params: params.to_json, headers: headers
     expect(response.status).to eq(200)
@@ -12,14 +14,6 @@ RSpec.describe '/api/v1/sessions' do
     user_data = JSON.parse(response.body, symbolize_names: true)
     expect(user_data).to be_a Hash
     expect(user_data[:status]).to eq(200)
+    expect(user_data).to have_key(:api_key)
   end
 end
-
-# Response:
-#
-# status: 200
-# body:
-#
-# {
-#   "api_key": "jgn983hy48thw9begh98h4539h4",
-# }
