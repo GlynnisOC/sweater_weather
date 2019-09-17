bda72c7cc52cbb9d52fdba35286301f9a061357e
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 08:03:53 -0600

    Refactor to one line in action

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::ForecastsController < ApplicationController
 
   def show
-    location = Location.geocoded(params['location'])
-    f_facade = ForecastFacade.new(location)
-    forecast = f_facade.forecast
-    render json: ForecastSerializer.new(forecast)
+    render json: ForecastSerializer.new(ForecastFacade.new(Location.geocoded(params['location'])).forecast)
   end
 end
