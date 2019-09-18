5022dda96bfa8b36222e4fd4b4aa455f7bf5b849
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-18 11:02:31 -0600

    Add future forecast method

--- app/services/darksky_service.rb
+++ app/services/darksky_service.rb
@@ -7,6 +7,21 @@ class DarkskyService
     get_json(url)
   end
 
+  def get_future_forecast(trip_points, trip_duration)
+    destination = Location.geocoded(trip_points[:destination])
+    lat = destination[:lat]
+    long = destination[:lng]
+    time = future_time(trip_duration)
+    url = "/forecast/#{ENV['DARKSKY-API-KEY']}/#{lat},#{long},#{time}"
+    get_json(url)
+  end
+
+  def future_time(trip_duration)
+    now = Time.now.to_i
+    future_time = trip_duration[:value]
+    now + future_time
+  end
+
   private
   def connection
     @_connection ||= Faraday.new("https://api.darksky.net")
