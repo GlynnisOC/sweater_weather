4094a7d21972ce8ba326ee82eeaaf89b5bf03426
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-18 09:06:10 -0600

    Add services to road trip facade

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -5,8 +5,21 @@ class RoadtripFacade
   end
 
   def expected_forecast
-    binding.pry
+    # binding.pry
     origin_lat_and_long = Location.geocoded(@trip_points[:origin])
     destination_lat_and_long = Location.geocoded(@trip_points[:destination])
+  end
+
+  # private
+  def ds_service
+    @_ds_service ||= DarkskyService.new
+  end
+
+  def geo_service
+    @_geo_service ||= GeocodeService.new
+  end
+
+  def dir_service
+    @_dir_service ||= DirectionService.new
   end
 end
