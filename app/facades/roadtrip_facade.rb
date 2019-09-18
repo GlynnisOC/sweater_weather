5b6c4958bded1e7554e3952a809065a48fb1e9b6
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-18 09:56:14 -0600

    Add trip duration method from service

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -6,11 +6,20 @@ class RoadtripFacade
 
   def expected_forecast
     # binding.pry
-    origin_lat_and_long = Location.geocoded(@trip_points[:origin])
-    destination_lat_and_long = Location.geocoded(@trip_points[:destination])
+    trip_duration
+
   end
 
-  # private
+  # def trip_points_lat_and_long
+  #   origin = Location.geocoded(@trip_points[:origin])
+  #   destination = Location.geocoded(@trip_points[:destination])
+  # end
+
+  def trip_duration
+    @_trip_duration ||= dir_service.get_drive_duration(@trip_points)
+  end
+
+  private
   def ds_service
     @_ds_service ||= DarkskyService.new
   end
