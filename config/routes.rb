c1e58f46d0758522a6497398592a4fcc6a6a4f41
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 21:38:01 -0600

    Add route for roadtrip controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   post 'api/v1/users', to: 'api/v1/users#create'
   post 'api/v1/sessions', to: 'api/v1/sessions#create'
+
+  post 'api/v1/road_trip', to: 'api/v1/roadtrip#show'
 end
