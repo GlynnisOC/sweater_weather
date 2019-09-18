7a20d48419855a142432c7e9c921a5230742c12c
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-17 21:38:22 -0600

    Add expectations for keys for response

--- spec/requests/api/v1/road_trip_forecast_spec.rb
+++ spec/requests/api/v1/road_trip_forecast_spec.rb
@@ -8,20 +8,20 @@ RSpec.describe '/api/v1/road_trip' do
     params = { "origin": "Denver,CO", "destination": "Pueblo,CO", "api_key": "#{user.api_key}" }
 
     post '/api/v1/road_trip', params: params.to_json, headers: headers
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq('application/json')
+
+    future_forecast = JSON.parse(response.body, symbolize_names: true)
+
+    expect(future_forecast).to be_a Hash
+    expect(future_forecast).to have_key(:temperature)
+    expect(future_forecast).to have_key(:summary)
+    expect(future_forecast).to have_key(:estimated_travel_time)
   end
 end
 
-
-# POST /api/v1/road_trip
-# Content-Type: application/json
-# Accept: application/json
-#
-# body:
-#
-
 # API key must be sent
 # If no API key or an incorrect key is provided return 401 (Unauthorized)
-# You will use the Google Directions API: https://developers.google.com/maps/documentation/directions/start
 # in order to find out how long it will take to travel from the two locations,
 # nd then deliver the weather forecast for the hour of arrival. For example, Denver
 #  to Pueblo would take two hours. You will deliver a forecast two hours in
