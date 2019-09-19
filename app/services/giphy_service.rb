2fc6bd9bad37674febbe8ab14e4481a331937975
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-19 07:04:51 -0600

    Change before save to after create as no other callbacks were working

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -1,33 +1,33 @@
-class GiphyService
-
-  def gifs(forecast)
-    conditions = forecast[:daily][:data]
-    day_objects = conditions.map do |daily|
-      Daily.new(daily)
-    end
-    all_summaries = day_objects.map { |day| day.summary }
-    all_gif_urls = []
-    5.times do
-      q = all_summaries.sample
-      url = "/v1/gifs/search"
-      params = { api_key: ENV['GIPHY-API-KEY'], q: q }
-      one_times_gif_url = get_json(url, params)[:data][0][:url]
-      all_gif_urls << one_times_gif_url
-    end
-    all_gif_urls.flat_map do |url|
-      day_objects.each do |daily|
-        daily.url = url
-      end
-    end
-  end
-
-  private
-  def connection
-    @_connection ||= Faraday.new("http://api.giphy.com")
-  end
-
-  def get_json(url, params)
-    response = connection.get(url, params)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-end
+# class GiphyService
+#
+#   def gifs(forecast)
+#     conditions = forecast[:daily][:data]
+#     day_objects = conditions.map do |daily|
+#       Daily.new(daily)
+#     end
+#     all_summaries = day_objects.map { |day| day.summary }
+#     all_gif_urls = []
+#     5.times do
+#       q = all_summaries.sample
+#       url = "/v1/gifs/search"
+#       params = { api_key: ENV['GIPHY-API-KEY'], q: q }
+#       one_times_gif_url = get_json(url, params)[:data][0][:url]
+#       all_gif_urls << one_times_gif_url
+#     end
+#     all_gif_urls.flat_map do |url|
+#       day_objects.each do |daily|
+#         daily.url = url
+#       end
+#     end
+#   end
+#
+#   private
+#   def connection
+#     @_connection ||= Faraday.new("http://api.giphy.com")
+#   end
+#
+#   def get_json(url, params)
+#     response = connection.get(url, params)
+#     JSON.parse(response.body, symbolize_names: true)
+#   end
+# end
