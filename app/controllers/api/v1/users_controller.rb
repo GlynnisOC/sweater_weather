6932c86b83ba70f1a2b55114523ffd856f26f74b
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-09-19 15:31:40 -0600

    Update users controller for new api key to be returned

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: { api_key: user.api_key, status: 201 }
+      render json: { api_key: user.new_api_key, status: 201 }
     else
       render json: { error: user.errors.full_messages.to_sentence, status: 400 }
     end
